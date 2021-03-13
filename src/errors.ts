f6a8d394cdac457f79f46e13f49c826f53f51138
Ibrahim Rahhal <ibrahimrahhal@Ibrahims-MacBook-Pro.local>
2021-03-13 16:46:31 +0200

    issue-14: Package Throws Vague Errors

--- src/errors.ts
+++ src/errors.ts
@@ -2,24 +2,24 @@ import FileController from "./file";
 import * as chalk from 'chalk';
 
 export class MultiExtendError extends Error {
-    static message:string = "Can't Extend Multiple Files";
+    static message = "Can't Extend Multiple Files";
     constructor() {
         super(MultiExtendError.message);
-                Object.setPrototypeOf(this, Object.create(MultiExtendError.prototype));
+        Object.setPrototypeOf(this, Object.create(MultiExtendError.prototype));
 
     }
 }
 
 export class MultiTerminateError extends Error {
-    static message:string = "Multiple Terminate Expressions Detected";
+    static message = "Multiple Terminate Expressions Detected";
     constructor() {
         super(MultiTerminateError.message);
         Object.setPrototypeOf(this, Object.create(MultiTerminateError.prototype));
     }
 }
 
-export const throwDescriptiveError = (error: any, file:FileController, content:string) => {
-    let errorMessage = file.formattedLineVisual(content, true, false, (file) => {
+export const throwDescriptiveError = (error: { new(): Error, message: string }, file:FileController, content:string): string => {
+    const errorMessage = file.formattedLineVisual(content, true, false, (file) => {
         return chalk.red(`Extending Failded. Error In File ${file.path}\n${error.message}\n`);
     });
     console.error(errorMessage);
